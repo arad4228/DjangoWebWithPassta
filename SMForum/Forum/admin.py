fcc4d65cfecf884392af52b0567a2cddce078ee6
arad4228 <arad4228@gmail.com>
2022-06-03 13:54:28 +0900

    Delete Project and Restart Project

--- SMForum/Forum/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Post
-from markdownx.admin import MarkdownxModelAdmin
-
-admin.site.register(Post)
--- SMForum/SMForum/settings.py
+++ SMForum/SMForum/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$aatmab-d8&7vz7yc%fdjr44hoj6!_8gp_e@du*8q^wd1uotmy'
+SECRET_KEY = '41x@%(tj^lste56^==2bru=iwz#g8nzd1rb_5b!_&wm^y$j-sh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'SMForum',
-    'Forum',
 ]
 
 MIDDLEWARE = [
@@ -105,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Seoul'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -120,4 +118,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
