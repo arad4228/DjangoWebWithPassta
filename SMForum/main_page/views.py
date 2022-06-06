95ee4ac1b890cf275f07da545a9fc540ee347876
arad4228 <arad4228@gmail.com>
2022-06-06 13:53:54 +0900

    Add Main_pages

---
+++ SMForum/main_page/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from forums.models import ForumPost, Category
+
+
+def landing(request):
+    return render(
+        request,
+        'main_page/landing.html',
+        {
+            'recent_posts': ForumPost.objects.order_by('-pk')[:3],
+            'Categories': Category.objects.all(),
+            'No_Categoriy_Post_count': ForumPost.objects.filter(category=None).count(),
+        }
+    )
