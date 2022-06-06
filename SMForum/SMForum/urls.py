6bb8350f5f0676a7ef82131827953cdd5098add3
arad4228 <arad4228@gmail.com>
2022-06-06 16:53:39 +0900

    Add Logout

--- SMForum/SMForum/urls.py
+++ SMForum/SMForum/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('forums/', include('forums.urls')),
     path('admin/', admin.site.urls),
--- SMForum/main_page/urls.py
+++ SMForum/main_page/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
+
 urlpatterns =[
+    path('logout/', auth_views.LogoutView.as_view()),
     path('', views.landing),
 ]
